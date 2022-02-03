5c58840dfb3861bb897faa84dc353bd91dbf636b
Geuni620 <dlrmsgnl620@naver.com>
2022-02-03 17:11:18 +0900

    백준 10869번

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-//boj.kr/1008
+//boj.kr/10869번
+//예제 입력 7 3
+//예쩨 출력 10 4 21 2 1
 const fs = require(`fs`);
-let input = fs.readFileSync(`/dev/stdin`).toString().split(` `);
-a = +input[0];
-b = +input[1];
+let input = fs.readFileSync(`dev/stdin`).toString().split(` `);
 
-console.log(a / b);
+A = +input[0];
+B = +input[1];
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(Math.floor(A / B));
+console.log(A % B);
