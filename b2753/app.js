10fb8f60c3638e7e99956cecd43dad99e418e7bb
Geuni <dlrmsgnl620@naver.com>
2022-02-14 23:35:24 +0900

    bkj review2

---
+++ b2753/app.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split(` `)
+  .map((item) => +item);
+
+solution(input);
+
+function solution(A) {
+  if ((A % 4 === 0 && A % 100 !== 0) || A % 400 === 0) {
+    console.log(1);
+  } else {
+    console.log(0);
+  }
+}
---
+++ b9498/app.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split(` `)
+  .map((item) => +item);
+
+solution(input);
+
+function solution(A) {
+  if (A >= 90) {
+    console.log("A");
+  } else if (A >= 80) {
+    console.log("B");
+  } else if (A >= 70) {
+    console.log("C");
+  } else if (A >= 60) {
+    console.log("D");
+  } else {
+    console.log("F");
+  }
+}
