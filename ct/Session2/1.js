083a2c16d7d74f27ffcb8e7e82828be4b5ad2a87
Geuni <dlrmsgnl620@naver.com>
2022-06-21 18:27:02 +0900

    Modify: TIL [복습]

--- ct/Session2/1.js
+++ ct/Session2/1.js
@@ -3,8 +3,8 @@ function solution(arr) {
   answer.push(arr[0]);
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < arr[i + 1]) {
-      answer.push(arr[i + 1]);
+    if (arr[i] > arr[i - 1]) {
+      answer.push(arr[i]);
     }
   }
 
