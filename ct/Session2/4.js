40eb898fe698ad221a62e01973cb3ec8685d714c
Geuni <dlrmsgnl620@naver.com>
2022-06-24 15:19:36 +0900

    Delete: 추후 복습을 위한 reset

--- ct/Session2/4.js
+++ ct/Session2/4.js
@@ -1,23 +1,6 @@
 function solution(arr) {
-  let answer = 0;
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) count = 0;
-    if (arr[i] === 1) count++;
-
-    answer += count;
-  }
-
-  console.log(count);
   return answer;
 }
 
-let arr = [1, 1, 1, 1, 1, 0, 1, 1, 0];
+let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
 console.log(solution(arr));
-
-/*
-#1. count를 하나 만들어줌
-#2. 1이면 count++, 0이면 count=0으로 계산해보기
-#3. answer에 count 누적
-*/
