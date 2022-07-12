8006463af3083bb7160ab3e430d7bda06b349ffd
Geuni <dlrmsgnl620@naver.com>
2022-07-12 22:03:26 +0900

    Modify: TIL[220712]

--- ct/Session4/1.js
+++ ct/Session4/1.js
@@ -1,47 +1,35 @@
 function solution(n, arr) {
   let answer = 0;
+  let tmp = 0;
+  let sum = 0;
   let max = Number.MIN_SAFE_INTEGER;
 
   for (let x of arr) {
-    let sum = 0;
-    let num = 0;
-
-    // while문 사용
-    // while (tmp) {
-    //   sum += tmp % 10;
-    //   tmp = Math.floor(tmp / 10);
-    // }
-
-    // 내장 메서드 사용
-    sum = x
-      .toString()
-      .split("")
-      .reduce((a, b) => a + Number(b), 0);
+    sum = 0;
+    tmp = x;
+    while (tmp) {
+      sum += tmp % 10;
+      tmp = parseInt(tmp / 10);
+    }
 
     if (max <= sum) {
       max = sum;
+      answer = x;
 
-      if (answer < x) answer = x;
+      if (max === sum) answer = Math.max(answer, x);
     }
   }
 
   return answer;
 }
 
-let arr = [119, 460, 637, 40, 551, 137, 123, 635];
+let arr = [128, 460, 603, 40, 521, 137, 123];
 console.log(solution(7, arr));
 
 /*
-#1. 자릿수의 합부터 구해보기, 세자릿수이므로 일의자리, 십의자리, 백의자리 각각 분해
-#2. String로 변경하고 index로 각각의 자릿수를 뽑아냄
-#3. 자릿수가 두자리일 수도 있고, 한 자리 일수도 있음. -> 고려해서 분기처리할 것
-#4. 자릿수를 각각 Number 변경하고 더해주기
-#5. max 값을 지정해주고 total이 max보다 클 경우 max 값을 total로 변경하고, 해당 arr[i]값을 answer로 담아주기
-#6. max값과 total 값이 같을 경우, answer에 담겨있는 값이 arr[i]보다 작을 경우 arr[i]를 answer로 변경, 그렇지 않을 시 answer return
-
----
-
-#1. 간소화 시켜보기
-#2. while문 적용
-#3. 내장메서드 사용해보기
+#1. answer는 가장 작은 수를 설정
+#2. for문으로 인자하나씩 받아주기
+#3. 한 자릿수씩 더해주기. -> while문으로 구현
+#4. max값보다 sum이 더 클 경우 max를 변경시켜주기
+#5. sum과 max 가 같다면 기존 x값이 더 큰 경우를 선택
 */
